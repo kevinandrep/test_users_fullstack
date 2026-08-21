0a0718b1f39a461cb1e4f6787ab6fb9a79ba1f68
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2026-08-20 19:14:02 -0500

    cors configuracion

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import { userRouter } from "./pods/user/index.js";
 import { errorHandler } from "./shared/middlewares/index.js";
+import cors from "cors";
 
 export const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 app.use(express.json());
 
 app.use("/api/users", userRouter);
